3c7d3b645a4f0573c9c0344238319aed3b942f0c
jim3692 <jim3692@gmail.com>
2019-08-05 15:01:46 +0300

    Fixed lesson notes images issues and added _id

--- graphql/resolvers/lesson.js
+++ graphql/resolvers/lesson.js
@@ -1,3 +1,4 @@
+const Image = require('../../models/image')
 const Lesson = require('../../models/lesson')
 const LessonNote = require('../../models/lessonNote')
 const User = require('../../models/user')
@@ -28,6 +29,14 @@ module.exports.lessonNotes = async (_, args, req) => {
     const lessonNotes = await LessonNote.find({ lesson: args.lesson })
     return lessonNotes.map(async lessonNote => ({
       ...transformData(lessonNote),
+      images: async () => {
+        const images = await Image.find({
+          _id: {
+            $in: lessonNote._doc.images
+          }
+        })
+        return images.map(img => transformData(img))
+      },
       lesson: async () => {
         const lesson = await Lesson.findOne({ _id: lessonNote._doc.lesson })
         return transformData(lesson)
@@ -64,15 +73,40 @@ module.exports.addLessonNotes = async (_, args, req) => {
   try {
     await req.user.checkAuthentication()
 
+    const images = await Image.insertMany(
+      args.lessonNote.images.map(img => ({
+        url: img.url,
+        uploader: req.user.id
+      }))
+    )
+
     const lessonNote = new LessonNote({
-      images: args.lessonNote.images,
+      images: images.map(img => img._doc._id),
       hypertexts: args.lessonNote.hypertexts,
       lesson: args.lessonNote.lesson,
       creator: req.user.id
     })
 
     const result = await lessonNote.save()
-    return transformData(result)
+    return {
+      ...transformData(result),
+      images: async () => {
+        const images = await Image.find({
+          _id: {
+            $in: result._doc.images
+          }
+        })
+        return images.map(img => transformData(img))
+      },
+      lesson: async () => {
+        const lesson = await Lesson.findById(result._doc.lesson)
+        return transformData(lesson)
+      },
+      creator: async () => {
+        const user = await User.findOne({ _id: result._doc.creator })
+        return transformData(user)
+      }
+    }
   } catch (err) {
     throw err
   }
