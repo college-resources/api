e0350ed57e00a4521d38538c3fa0b480e8a580ad
jim3692 <jim3692@gmail.com>
2019-08-24 14:19:38 +0300

    Fixed AuthenticationError not being thrown

--- modules/logger.js
+++ modules/logger.js
@@ -1,4 +1,10 @@
+const { AuthenticationError } = require('apollo-server-express')
+
 module.exports = err => {
+  if (err instanceof AuthenticationError) {
+    throw err
+  }
+
   if (process.env.NODE_ENV === 'development') {
     console.trace(err)
   } else {
