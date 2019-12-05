d01504801d5dd172875079b3c152b8cabf15e980
jim3692 <jim3692@gmail.com>
2019-12-05 12:56:08 +0200

    Removed authentication routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const authRouter = require('./auth')
-
 const router = express.Router()
 
 router.use(bodyParser.json())
-router.use('/auth', authRouter)
 
 module.exports = router
