5eb0bcf91addcbb64ca64dda17978a3798fbbee7
feedmycat <konstantangelou@gmail.com>
2020-09-24 00:17:36 +0300

    Add preference schema

---
+++ models/preference.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const preferenceSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    feeding: {
+      type: Schema.Types.ObjectId,
+      ref: 'Feeding',
+      required: false
+    },
+    department: {
+      type: Schema.Types.ObjectId,
+      ref: 'Department',
+      required: false
+    },
+    semester: {
+      type: Number,
+      required: false
+    },
+    courses: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Lesson',
+      required: false
+    }],
+    theme: {
+      type: String,
+      required: false
+    }
+  }
+)
+
+module.exports = mongoose.model('Preference', preferenceSchema)
