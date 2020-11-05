941d64a768dc42195f3b4c57bd74fe2211ab7044
jim3692 <jim3692@gmail.com>
2020-11-06 00:16:15 +0200

    Update dependencies - Remove Webpack

--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -2,10 +2,7 @@ const path = require('path')
 const { importSchema } = require('graphql-import')
 const { gql } = require('apollo-server-express')
 
-module.exports = (options, loaderContext) => {
-  const schema = importSchema(path.join(__dirname, './schema.graphql'))
-  const ast = gql`${schema}`
-  const astString = JSON.stringify(ast)
+const schema = importSchema(path.join(__dirname, './schema.graphql'))
+const ast = gql`${schema}`
 
-  return { code: `module.exports = ${astString}` }
-}
+module.exports = ast
