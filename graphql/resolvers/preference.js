62b6427633049ecc7942085a9ed06c4ac3863638
feedmycat <konstantangelou@gmail.com>
2020-11-07 00:00:04 +0200

    Add addPreference mutation

--- graphql/resolvers/preference.js
+++ graphql/resolvers/preference.js
@@ -20,3 +20,38 @@ module.exports.preference = async (_, args, req) => {
     logger(err)
   }
 }
+
+module.exports.addPreference = async (_, args, req) => {
+  try {
+    await req.user.checkAuthentication()
+
+    const request = {}
+
+    if (args.preference.feeding) {
+      request.feeding = args.preference.feeding
+    }
+    if (args.preference.department) {
+      request.department = args.preference.department
+    }
+    if (args.preference.semester) {
+      request.semester = args.preference.semester
+    }
+    if (args.preference.courses) {
+      request.courses = args.preference.courses
+    }
+    if (args.preference.theme) {
+      request.theme = args.preference.theme
+    }
+
+    const result = await Preference.update(
+      { user: req.user.id },
+      { $set: request },
+      { new: true }
+    )
+    // console.log(result._doc)
+    // req.loaders.preference.prime(result.id, result)
+    return transformPreference(req.loaders, result)
+  } catch (err) {
+    logger(err)
+  }
+}
